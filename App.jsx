282438307da0338c0ef6b967349e7e53b3ae4ba3
Mutsumix <mkajihara.dev@gmail.com>
2023-04-20 17:20:57 +0900

    Create Hello component

--- App.jsx
+++ App.jsx
@@ -2,6 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello bang style={{fontSize: 16}}>World</Hello>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -10,13 +24,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-    </View>
-  );
-}
